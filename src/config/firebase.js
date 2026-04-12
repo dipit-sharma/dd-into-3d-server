94de0cb06075bb479bc4a708d281edeee33ab3e8
superstark02@gmail.com <superstark02@gmail.com>
2026-04-12 21:32:24 +0530

    firebase config added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,19 @@
 const admin = require("firebase-admin");
 
+var serviceAccount = require("./firebase.json");
+
 if (!admin.apps.length) {
+  //   admin.initializeApp({
+  //     credential: admin.credential.cert({
+  //       projectId: process.env.FIREBASE_PROJECT_ID,
+  //       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  //       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  //     }),
+  //     storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+  //   });
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
-    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 

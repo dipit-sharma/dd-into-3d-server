c0ad93683a7360d3671247bd2a206a71839b70bb
superstark02@gmail.com <superstark02@gmail.com>
2026-04-15 23:14:53 +0530

    fixed for deployment

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,13 @@
+import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({
-    path: path.resolve(__dirname, "../.env"),
-});
+const envPaths = [
+    path.resolve(process.cwd(), ".env"),
+    path.resolve(__dirname, "../.env"),
+    path.resolve(__dirname, "../../.env"),
+];
+
+const envPath = envPaths.find((candidatePath) => fs.existsSync(candidatePath));
+
+dotenv.config(envPath ? { path: envPath } : undefined);
